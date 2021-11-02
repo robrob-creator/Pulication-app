f06219d87b723836938ca6b286d83f1309747dd7
robrob-creator <60162796+robrob-creator@users.noreply.github.com>
2021-11-02 07:23:37 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -42,6 +42,12 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client', 'index.html'));
+});
+
+app.listen(process.env.PORT ||5000, () => {
   console.log("Backend is running.");
 });
