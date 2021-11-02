cdb737f0587ae8f505c1f3f745402586eaac2c3c
robrob-creator <60162796+robrob-creator@users.noreply.github.com>
2021-11-02 08:17:36 +0700

    UI adjustments

--- api/client/src/pages/home/Home.jsx
+++ api/client/src/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
-import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
 import {axiosInstance} from "../../config";
 import { useLocation } from "react-router";
