12413c07cc49fb8e67409c581f0c50e49b197fa2
robrob-creator <60162796+robrob-creator@users.noreply.github.com>
2021-11-02 07:40:19 +0700

    Update config.js

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export const axiosInstance =axios.create({
-    baseURL: "https://publication-app.herokuapp.com/"
+    baseURL: "https://publication-app.herokuapp.com/api/"
 })
